054d1130d3580ba0073fed162024a78c2d4dc5f1
Rohit P Modani <rohitpmodani@gmail.com>
2025-04-30 00:03:19 +0530

    Prepare for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root:'.',
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',
+  },
+  build: {
+    outDir: 'dist',
   },
 })
