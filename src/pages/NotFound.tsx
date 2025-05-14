8feeab4c992fea15b3618a6925a841d42245ff8e
Rohit P Modani <rohitpmodani@gmail.com>
2025-05-15 00:04:02 +0530

    Few more minor changes

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -9,6 +9,7 @@ function NotFound() {
         <div className="p-8 text-center">
           <h1 className="text-3xl font-bold mb-4">404 - Page Not Found</h1>
           <p className="text-gray-600">Oops! The page you’re looking for doesn’t exist.</p>
+          <p> You can raise any new requests on Github under Issues section</p>
         </div>
       </div>
     );
